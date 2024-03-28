339dffc0ed500f5b05dd096c41f5444c4e692ef6
Ben Slattery <ben@benslattery.com>
2024-03-28 23:10:22 +1300

    visually better and corrected bad display logic

--- src/main.cpp
+++ src/main.cpp
@@ -28,19 +28,38 @@ int main()
    std::getline(std::cin, input);
    auto tree = AST::createTree(input);
 
-   const float WIDTH = 200.f;
-   const float HEIGHT = 200.f;
-   sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "Graphing calculator");
+   const float WIDTH = 400.f;
+   const float HEIGHT = 400.f;
+   sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "Graphing calculator", sf::Style::Close | sf::Style::Titlebar);
 
    const int POINTS = 195; //  arbitrary
 
-   sf::VertexArray axes{sf::Lines, 4};
+   const int X_RANGE = 10;
+   const int Y_RANGE = 10;
+   sf::VertexArray axes{sf::Lines, 4 + 2*(X_RANGE-1) + 2*(Y_RANGE-1)};
    auto grey = sf::Color{0x7f7f7fff};
-   axes[0] = sf::Vertex{sf::Vector2f{0, HEIGHT/2}, grey};
-   axes[1] = sf::Vertex{sf::Vector2f{WIDTH, HEIGHT/2}, grey};
-   axes[2] = sf::Vertex{sf::Vector2f{WIDTH/2, 0}, grey};
-   axes[3] = sf::Vertex{sf::Vector2f{WIDTH/2, HEIGHT}, grey};
+   auto darkgrey = sf::Color{0x3f3f3fff};
 
+   int axesindex = 0;
+
+   for (int x = 1; x < X_RANGE; x++)
+   {
+      if (x == X_RANGE / 2) continue;
+      axes[axesindex++] = sf::Vertex{sf::Vector2f{x * WIDTH/X_RANGE, 0}, darkgrey};
+      axes[axesindex++] = sf::Vertex{sf::Vector2f{x * WIDTH/X_RANGE, HEIGHT}, darkgrey};
+   }
+
+   for (int y = 1; y < Y_RANGE; y++)
+   {
+      if (y == Y_RANGE / 2) continue;
+      axes[axesindex++] = sf::Vertex{sf::Vector2f{0, y * HEIGHT/Y_RANGE}, darkgrey};
+      axes[axesindex++] = sf::Vertex{sf::Vector2f{WIDTH, y * HEIGHT/Y_RANGE}, darkgrey};
+   }
+
+   axes[axesindex++] = sf::Vertex{sf::Vector2f{0, HEIGHT/2}, grey};
+   axes[axesindex++] = sf::Vertex{sf::Vector2f{WIDTH, HEIGHT/2}, grey};
+   axes[axesindex++] = sf::Vertex{sf::Vector2f{WIDTH/2, 0}, grey};
+   axes[axesindex++] = sf::Vertex{sf::Vector2f{WIDTH/2, HEIGHT}, grey};
 
    sf::VertexArray line{sf::LinesStrip, POINTS};
    for (int p = 0; p < POINTS; p++)
@@ -48,10 +67,10 @@ int main()
       float screen_x = p*(WIDTH / POINTS);
 
       // screen goes from x {-5 to 5}
-      float x = (screen_x / WIDTH) * 10.f - 5.f;
+      float x = (p / static_cast<float>(POINTS)) * X_RANGE - 5.f;
 
       // screen goes from y {-5 to 5}
-      float y = (HEIGHT/2) - 10*tree->evaluate(x); // y is flipped in graphics
+      float y = (HEIGHT/2) - (HEIGHT/Y_RANGE)*tree->evaluate(x); // y is flipped in graphics
 
       line[p] = sf::Vertex{sf::Vector2f{screen_x, y}, sf::Color::White};
    }
